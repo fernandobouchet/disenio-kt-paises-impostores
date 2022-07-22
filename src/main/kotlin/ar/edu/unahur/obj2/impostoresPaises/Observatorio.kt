7dfd99eb920292d243b4c7ca767e0e88436aaf73
fernandobouchet <fernandobouchet@gmail.com>
2022-07-21 22:23:29 -0300

    Agregadas alguna excepciones en 'Observatorio'

--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Observatorio.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Observatorio.kt
@@ -1,16 +1,16 @@
 package ar.edu.unahur.obj2.impostoresPaises
 
 object Observatorio {
-    val paises = mutableListOf<Pais>()
+    private val paisesRegistrados = mutableListOf<Pais>()
 
     fun registrarPais(pais: Pais) {
-        paises.add(pais)
+        paisesRegistrados.add(pais)
     }
 
     private fun paisQueRepresenta(pais: String): Pais? {
-        return if(paises.isNotEmpty() && paises.any { x -> x.nombre == pais }) {
-            paises.find { a -> a.nombre == pais }
-        } else throw Exception("El pais indicado no se encuentra registrado en el observatorio")
+        return if(paisesRegistrados.isNotEmpty() && paisesRegistrados.any { x -> x.nombre == pais }) {
+            paisesRegistrados.find { a -> a.nombre == pais }
+        } else throw Exception("Uno de los paises indicado no se encuentra registrado en el observatorio.")
     }
 
     fun sonLimitrofes(pais1: String, pais2: String): Boolean {
@@ -34,19 +34,26 @@ object Observatorio {
     }
 
     fun codigoIsoPaisesMasPoblacion(): List<String> {
-        return paises.sortedByDescending { it.densidadPoblacional() }
-            .take(5)
-            .map { pais -> pais.codigoIso3 }
+        if (paisesRegistrados.size >= 5) {
+            return paisesRegistrados.sortedByDescending { it.densidadPoblacional() }
+                .take(5)
+                .map { pais -> pais.codigoIso3 }
+        } else throw Exception("No se encuentran en el observatorio el mínimo de 5 paises necesarios para esta función.")
     }
 
     fun continenteConMasPaisesPlurinacionales(): String {
-        return paises.filter { pais -> pais.esPlurinacional() }
-            .groupBy { it.continente }
-            .maxByOrNull { it.value.size }!!.key
+        val paisesPlurincionales = paisesRegistrados.filter { pais -> pais.esPlurinacional() }
+        if(paisesPlurincionales.isNotEmpty()) {
+            return paisesPlurincionales
+                .groupBy { it.continente }
+                .maxByOrNull { it.value.size }!!.key
+        } else throw Exception("No se encontraron paises plurinacionales en el observatorio")
     }
 
-    fun promedioDensidadPoblacionalInsulares(): Any {
-        val paisesInsulares =  paises.filter { pais -> pais.esUnaIsla() }
-        return paisesInsulares.sumBy { it.densidadPoblacional() } / paisesInsulares.size
+    fun promedioDensidadPoblacionalInsulares(): Int {
+        val paisesInsulares =  paisesRegistrados.filter { pais -> pais.esUnaIsla() }
+        if(paisesInsulares.isNotEmpty()) {
+            return paisesInsulares.sumBy { it.densidadPoblacional() } / paisesInsulares.size
+        } else throw Exception("No se encontraron paises insulares en el observatorio.")
     }
 }
